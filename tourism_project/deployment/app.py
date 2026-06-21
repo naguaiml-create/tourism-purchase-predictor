e23c810dc3a57ebb3fb946b159e1482ac45336a1
naguaiml-create <naguaiml@gmail.com>
2026-06-21 09:14:39 +0000

    Tourism Project commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+saved_model_path = "tourism_project/deployment_files/best_machine_tourism_predit_model.joblib"
+joblib.dump(best_model, saved_model_path)
+
+print(f"Model saved successfully at {saved_model_path}")
+
+# Download and load the model
+model_path = hf_hub_download(repo_id="kottanagu/smart-tourism-customer-prediction-platform", filename="best_machine_tourism_predit_model.joblib")
+model = joblib.load(model_path)
+
+# Streamlit UI for Tourism Package Purchase Prediction
+st.title("Wellness Tourism Package Purchase Prediction Using an End-to-End MLOps Pipeline")
+st.write("""
+This application uses machine learning to predict the likelihood of a customer purchasing the Wellness Tourism Package.
+""")
+
+# User input
+Age = st.number_input("Age", min_value=25, step=1, value=1)
+Type_of_Contact = st.selectbox("Type of Contact", ["Company Invited", "Self Enquiry"])
+City_Tier = st.selectbox("City Tier", ["1", "2", "3"])
+Duration_of_Pitch = st.number_input("Duration of Pitch", min_value=20, step=1, value=1)
+Occupation = st.selectbox("Occupation", ["Salaried", "Small Business", "Large Business", "Free Lancer"])
+Gender = st.selectbox("Gender", ["Male", "Female"])
+Number_of_Persons_Visiting = st.number_input("Number of Persons Visiting", min_value=2, step=1, value=1)
+Number_of_Followups = st.number_input("Number of Followups", min_value=2, step=1, value=1)
+Product_Pitched = st.selectbox("Product Pitched", ["Basic", "Standard", "Deluxe", "Super Deluxe", "King"])
+Preferred_Property_Star = st.selectbox("Preferred Property Star", ["3", "4", "5"])
+Marital_Status = st.selectbox("Marital Status", ["Single", "Married", "Divorced"])
+Number_Of_Trips = st.number_input("Number of Trips", min_value=2, step=1, value=1)
+Passport = st.selectbox("Passport", ["Yes", "No"])
+Pitch_Satisfaction_Score = st.selectbox("Pitch Satisfaction Score", ["1", "2", "3", "4", "5"])
+Own_Car = st.selectbox("Own Car", ["Yes", "No"])
+Number_Of_Children_Visiting = st.number_input("Number of Children Visiting", min_value=1, step=1, value=1)
+Designation = st.selectbox("Designation", ["Executive", "Manager", "Senior Manager",  "AVP", "VP"])
+Monthly_Income = st.number_input("Monthly Income", min_value=25000, step=100, value=1000)
+
+
+# Assemble input into DataFrame
+input_data = pd.DataFrame([{
+    'Age': Age,
+    'Type_of_Contact': Type_of_Contact,
+    'City_Tier': City_Tier,
+    'Duration_of_Pitch': Duration_of_Pitch,
+    'Occupation': Occupation,
+    'Gender': Gender,
+    'Number_of_Persons_Visiting': Number_of_Persons_Visiting,
+    'Number_of_Followups': Number_of_Followups,
+    'Product_Pitched': Product_Pitched,
+    'Preferred_Property_Star': Preferred_Property_Star,
+    'Marital_Status': Marital_Status,
+    'Number_Of_Trips': Number_Of_Trips,
+    'Passport': Passport,
+    'Pitch_Satisfaction_Score': Pitch_Satisfaction_Score,
+    'Own_Car': Own_Car,
+    'Number_Of_Children_Visiting': Number_Of_Children_Visiting,
+    'Designation': Designation,
+    'Monthly_Income': Monthly_Income
+}])
+
+
+if st.button("Predict Purchase"):
+    prediction = model.predict(input_data)[0]
+    result = "Tourism Package Purchase Prediction" if prediction == 1 else "No Failure"
+    st.subheader("Prediction Result:")
+    st.success(f"The model predicts: **{result}**")
---
+++ tourism_project/model_building/prep.py
@@ -0,0 +1,61 @@
+# for data manipulation
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for converting text data in to numerical representation
+from sklearn.preprocessing import LabelEncoder
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi, create_repo
+
+# Define constants for the dataset and output paths
+api = HfApi(token=os.getenv("HF_TOKEN"))
+DATASET_PATH = "hf://datasets/kottanagu/smart-tourism-customer-prediction-platform/tourism.csv"
+df = pd.read_csv(DATASET_PATH)
+print("Dataset loaded successfully.")
+
+# Drop the unique identifier
+df.drop(columns=['CustomerID'], inplace=True)
+
+#Corrected Gender values by merging "Fe Male" into "Female".
+df['Gender'] = df['Gender'].str.replace(' ', '').str.capitalize()
+
+#Creating categories from Age and Monthly Income
+df['age_group'] = pd.cut(
+    x=df['age'],
+    bins=[18, 30, 40, 50, 61],
+    labels=["18-30", "31-40", "41-50", "51-60", "60+"],
+)
+
+# Standardize marital status values by replacing 'Unmarried' with 'Single'
+df['MaritalStatus'] = df['MaritalStatus'].replace('Unmarried', 'Single')
+
+
+target_col = 'ProdTaken'
+
+# Split into X (features) and y (target)
+X = df.drop(columns=[target_col])
+y = df[target_col]
+
+# Perform train-test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id="kottanagu/smart-tourism-customer-prediction-platform",
+        repo_type="dataset",
+    )
